7f75acff1ec5ffc9b8054deb18bc170de669d3a6
babydance <taoshanchang@foxmail.com>
2015-11-04 15:24:12 +0800

    [update]action模板

---
+++ nebula-service/src/main/java/com/olymtech/nebula/service/action/CpEtcAction.java
@@ -0,0 +1,34 @@
+/**
+ * Olymtech.com Inc.
+ * Copyright (c) 2002-2015 All Rights Reserved
+ */
+package com.olymtech.nebula.service.action;
+
+import com.olymtech.nebula.core.action.AbstractAction;
+import com.olymtech.nebula.entity.NebulaPublishEvent;
+
+/**
+ * @author taoshanchang 15/11/4
+ */
+public class CpEtcAction extends AbstractAction {
+
+
+
+    public CpEtcAction() {
+    }
+
+    @Override
+    public boolean doAction(NebulaPublishEvent event) throws Exception {
+
+
+
+
+
+        return false;
+    }
+
+    @Override
+    public void doFailure(NebulaPublishEvent event) {
+
+    }
+}
