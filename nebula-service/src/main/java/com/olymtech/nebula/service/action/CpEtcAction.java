323fbbe85adb783feb5a6384c1cdf387092183e3
babydance <taoshanchang@foxmail.com>
2015-11-05 10:50:15 +0800

    [add]action IOC 注入解决方案

--- nebula-service/src/main/java/com/olymtech/nebula/service/action/CpEtcAction.java
+++ nebula-service/src/main/java/com/olymtech/nebula/service/action/CpEtcAction.java
@@ -6,13 +6,18 @@ package com.olymtech.nebula.service.action;
 
 import com.olymtech.nebula.core.action.AbstractAction;
 import com.olymtech.nebula.entity.NebulaPublishEvent;
+import com.olymtech.nebula.service.IPublishEventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
  * @author taoshanchang 15/11/4
  */
+@Service
 public class CpEtcAction extends AbstractAction {
 
-
+    @Autowired
+    private IPublishEventService publishEventService;
 
     public CpEtcAction() {
     }
@@ -20,11 +25,11 @@ public class CpEtcAction extends AbstractAction {
     @Override
     public boolean doAction(NebulaPublishEvent event) throws Exception {
 
+        System.out.println("CpEtcAction");
+        System.out.println(publishEventService);
 
 
-
-
-        return false;
+        return true;
     }
 
     @Override
--- nebula-service/src/main/java/com/olymtech/nebula/service/action/CreateDirAciton.java
+++ nebula-service/src/main/java/com/olymtech/nebula/service/action/CreateDirAciton.java
@@ -6,12 +6,19 @@ package com.olymtech.nebula.service.action;
 
 import com.olymtech.nebula.core.action.AbstractAction;
 import com.olymtech.nebula.entity.NebulaPublishEvent;
+import com.olymtech.nebula.service.IPublishEventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
  * @author taoshanchang 15/11/4
  */
+
+@Service
 public class CreateDirAciton extends AbstractAction {
 
+    @Autowired
+    private IPublishEventService publishEventService;
 
 
     public CreateDirAciton() {
@@ -20,7 +27,13 @@ public class CreateDirAciton extends AbstractAction {
 
     @Override
     public boolean doAction(NebulaPublishEvent event) throws Exception {
-        return false;
+        System.out.println("CreateDirAciton");
+        System.out.println(publishEventService);
+
+
+
+
+        return true;
     }
 
     @Override
