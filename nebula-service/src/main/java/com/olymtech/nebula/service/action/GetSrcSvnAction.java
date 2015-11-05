267937568f103306e2b80b6b5582d5748a96520b
胡宗鑫 <zongxinh@olymtech.com>
2015-11-05 21:16:36 +0800

    [fix]svn测试

--- nebula-service/src/main/java/com/olymtech/nebula/service/action/GetSrcSvnAction.java
+++ nebula-service/src/main/java/com/olymtech/nebula/service/action/GetSrcSvnAction.java
@@ -7,22 +7,25 @@ package com.olymtech.nebula.service.action;
 import com.olymtech.nebula.core.action.AbstractAction;
 import com.olymtech.nebula.core.svn.SvnUtils;
 import com.olymtech.nebula.entity.NebulaPublishEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.tmatesoft.svn.core.wc.SVNClientManager;
 
 /**
  * Created by Gavin on 2015-11-04 20:12.
  */
 public class GetSrcSvnAction extends AbstractAction {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Override
     public boolean doAction(NebulaPublishEvent event) throws Exception {
-        String svnUrl = event.getPublishSvn()+"/"+event.getPublishEnv();
+        String svnUrl = event.getProductSrcSvn()+"/"+event.getPublishEnv();
         SVNClientManager svnClientManager = SvnUtils.createSvnClientManager(svnUrl, "gavin", "hellomonitor");
         try{
             SvnUtils.checkout(svnClientManager,svnUrl,"/Users/saas/deploy_tmp/" + event.getPublishProductKey() + "/src_svn/");
             return true;
         }catch (Exception e){
-//            logger.error("GetPublishSvnAction error:",e);
+            logger.error("GetPublishSvnAction error:",e);
         }
         return false;
     }
