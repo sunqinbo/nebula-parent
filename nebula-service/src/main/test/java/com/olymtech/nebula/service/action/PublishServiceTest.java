1bf335054491948636658de4a171e17835da3313
babydance <taoshanchang@foxmail.com>
2015-11-05 21:07:04 +0800

    [add]war包分发Action

--- nebula-service/src/main/test/java/com/olymtech/nebula/service/action/PublishServiceTest.java
+++ nebula-service/src/main/test/java/com/olymtech/nebula/service/action/PublishServiceTest.java
@@ -4,20 +4,11 @@
  */
 package java.com.olymtech.nebula.service.action;
 
-import com.olymtech.nebula.core.action.ActionChain;
-import com.olymtech.nebula.core.action.Dispatcher;
-import com.olymtech.nebula.core.utils.SpringUtils;
-import com.olymtech.nebula.entity.NebulaPublishEvent;
-import com.olymtech.nebula.service.action.CpEtcAction;
-import com.olymtech.nebula.service.action.CreateDirAciton;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
-
-import javax.annotation.Resource;
 
 /**
  * Created by Gavin on 2015-11-05 16:21.
