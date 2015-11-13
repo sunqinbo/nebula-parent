05a9d1f2f44d7bdcda9a5f66a9dd31acd0df8d9e
babydance <taoshanchang@foxmail.com>
2015-11-13 13:45:19 +0800

    salt读取配置方式修改，修复文件流没有关闭

--- nebula-web/src/main/java/com/olymtech/nebula/controller/PublishEventController.java
+++ nebula-web/src/main/java/com/olymtech/nebula/controller/PublishEventController.java
@@ -4,34 +4,21 @@ import com.olymtech.nebula.core.action.Action;
 import com.olymtech.nebula.core.action.ActionChain;
 import com.olymtech.nebula.core.action.Dispatcher;
 import com.olymtech.nebula.core.utils.SpringUtils;
-import com.olymtech.nebula.entity.Callback;
-import com.olymtech.nebula.entity.NebulaPublishEvent;
-import com.olymtech.nebula.entity.NebulaPublishSchedule;
-import com.olymtech.nebula.entity.ProductTree;
+import com.olymtech.nebula.entity.*;
 import com.olymtech.nebula.entity.enums.PublishAction;
 import com.olymtech.nebula.entity.enums.PublishActionGroup;
-import com.olymtech.nebula.service.IAnalyzeArsenalApiService;
-import com.olymtech.nebula.entity.*;
-import com.olymtech.nebula.service.IPublishEventService;
-import com.olymtech.nebula.service.IPublishHostService;
-import com.olymtech.nebula.service.IPublishScheduleService;
-import com.olymtech.nebula.service.IPublishSequenceService;
+import com.olymtech.nebula.service.*;
 import com.olymtech.nebula.service.action.*;
 import org.apache.commons.lang.StringUtils;
-import org.omg.PortableInterceptor.INACTIVE;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -64,8 +51,7 @@ public class PublishEventController extends BaseController{
     }
 
     @RequestMapping(value="/publish_event/getProductTreeListByPid.htm",method= {RequestMethod.POST,RequestMethod.GET})
-    @ResponseBody
-    public Callback getProductTreeListByPid(Integer pid){
+    public @ResponseBody Callback getProductTreeListByPid(Integer pid){
         try{
             List<ProductTree> productTrees = analyzeArsenalApiService.getProductTreeListByPid(pid);
             return returnCallback("Success",productTrees);
