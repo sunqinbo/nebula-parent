6084b9596760972c75301fa3c8fade475b6580d7
胡宗鑫 <zongxinh@olymtech.com>
2015-11-16 21:35:58 +0800

    [add]增加重试的请求

--- nebula-web/src/main/webapp/WEB-INF/public/js/main.js
+++ nebula-web/src/main/webapp/WEB-INF/public/js/main.js
@@ -185,3 +185,24 @@ nebula.publish.process.publishContinue = function(){
         }
     });
 };
+
+nebula.publish.process.retryPublishRollback = function(){
+    var id = $("#eventId").val();
+    $.ajax({
+        url:"/publish_event/retryPublishRollback.htm",
+        type:"post",
+        data:{"id":id},
+        success:function(jsonData){
+            if(jsonData.callbackMsg.match(/Success/)){
+                window.location.href="/publishProcess.htm?id="+id;
+                $.notify({
+                    icon: '',
+                    message: "正式发布成功"
+                },{
+                    type: 'info',
+                    timer: 2000
+                });
+            }
+        }
+    });
+};
