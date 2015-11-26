95750adab8df6c74c64c859cf094131526d0c970
wenjil <wenjil@olymtech.com>
2015-11-26 10:24:37 +0800

    [add]用户，权限，角色基本表单验证

--- nebula-web/src/main/webapp/WEB-INF/public/js/createPermission.js
+++ nebula-web/src/main/webapp/WEB-INF/public/js/createPermission.js
@@ -1,4 +1,24 @@
 $(function(){
+
+    $("#insertform").validate({
+        rules:{
+            permissionName:{required:true},
+            permissionCname:{required:true},
+            permission:{required:true},
+            permissionDesc:{required:true},
+            permissionType:{required:true},
+            isEnable:{required:true},
+        },
+        messages:{
+            permissionName:{required:"权限名称不能为空"},
+            permissionCname:{required:"权限中文名称不能为空"},
+            permission:{required:"权限不能为空"},
+            permissionDesc:{required:"权限描述不能为空"},
+            permissionType:{required:"权限类型不能为空"},
+            isEnable:{required:"请选择是否启用"}
+        }
+    });
+
     $("#permissionType").change(function(){
         if($("#permissionType option:selected").text()=="按钮"){
             $.ajax({
@@ -28,31 +48,33 @@ $(function(){
         }
     })
     $("#submit").click(function(){
-        $.ajax({
-            type: "POST",
-            url:"/permission/add",
-            data:$('#insertform').serialize(),
-            async: false,
-            success: function(data) {
-                $.notify({
-                    icon: '',
-                    message: "添加权限成功"
+        if($("#insertform").valid()) {
+            $.ajax({
+                type: "POST",
+                url: "/permission/add",
+                data: $('#insertform').serialize(),
+                async: false,
+                success: function (data) {
+                    $.notify({
+                        icon: '',
+                        message: "添加权限成功"
 
-                },{
-                    type: 'info',
-                    timer: 1000
-                });
-            },
-            error: function(request) {
-                $.notify({
-                    icon: '',
-                    message: "添加权限失败"+ errorThrown
+                    }, {
+                        type: 'info',
+                        timer: 1000
+                    });
+                },
+                error: function (request) {
+                    $.notify({
+                        icon: '',
+                        message: "添加权限失败" + errorThrown
 
-                },{
-                    type: 'info',
-                    timer: 1000
-                });
-            }
-        });
+                    }, {
+                        type: 'info',
+                        timer: 1000
+                    });
+                }
+            });
+        }
     })
 })
