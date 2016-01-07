a2a8529ee5d3872ce610223038875d4bea01c178
wenjil <wenjil@olymtech.com>
2016-01-07 13:36:24 +0800

    [update]刷新CND显示

--- nebula-web/src/main/webapp/WEB-INF/public/assets/js/publishProcess.js
+++ nebula-web/src/main/webapp/WEB-INF/public/assets/js/publishProcess.js
@@ -53,21 +53,74 @@ $(document).ready(function(){
             url: "/publish/list/describeRefreshTasks",
             dataType:"json",
             success: function (data) {
-                var modal_tbString=""
-                for(var i= 0,modallen=data.responseContext.tasks.length;i<modallen;i++ ){
-                    var task=data.responseContext.tasks[i];
-                    modal_tbString+="<tr><td>"+task.taskId+"</td>" +
-                        "<td>"+task.objectPath+"</td>"+
-                        "<td>"+task.status+"</td>"+
-                        "<td>"+task.creationTime+"</td>"+
-                        "</tr>"
+                if(data.callbackMsg=="Success") {
+                    var modal_tbString = ""
+                    for (var i = 0, modallen = data.responseContext.tasks.length; i < modallen; i++) {
+                        var task = data.responseContext.tasks[i];
+                        modal_tbString += "<tr><td>" + task.taskId + "</td>" +
+                            "<td>" + task.objectPath + "</td>" +
+                            "<td>" + task.status + "</td>" +
+                            "<td>" + task.creationTime + "</td>" +
+                            "</tr>"
+                    }
+                    $("#modal_tb").html(modal_tbString);
                 }
-                $("#modal_tb").html(modal_tbString);
+                else{
+                    $.notify({
+                        icon: '',
+                        message: data.responseContext
+                    }, {
+                        type: 'danger',
+                        timer: 1000
+                    });
+                }
             },
             error: function (XMLHttpRequest, textStatus, errorThrown) {
                 $.notify({
                     icon: '',
                     message: "请求CDN列表失败，原因" + errorThrown
+                }, {
+                    type: 'danger',
+                    timer: 1000
+                });
+            }
+        })
+    });
+
+    $("#modalSubmit_btn").click(function(){
+        $("#modalUrl_select").find("option:selected").text()
+        $.ajax({
+            type:"POST",
+            url: "/publish/add/refreshObjectCaches",
+            data:{
+                objectPath:$("#modalUrl_select").find("option:selected").text(),
+                objectType:"Directory",
+            },
+            dataType:"json",
+            success: function (data) {
+                if(data.callbackMsg=="Error"){
+                    $.notify({
+                        icon: '',
+                        message: data.responseContext
+                    }, {
+                        type: 'danger',
+                        timer: 1000
+                    });
+                }
+                else {
+                    $.notify({
+                        icon: '',
+                        message: "刷新CDN成功"
+                    }, {
+                        type: 'info',
+                        timer: 1000
+                    });
+                }
+            },
+            error: function (XMLHttpRequest, textStatus, errorThrown) {
+                $.notify({
+                    icon: '',
+                    message: "刷新CDN失败，原因" + errorThrown
                 }, {
                     type: 'danger',
                     timer: 1000
