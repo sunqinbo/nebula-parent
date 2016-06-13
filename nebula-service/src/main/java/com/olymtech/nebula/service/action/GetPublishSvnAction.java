a2e92fa4506f4293478d22b4784e3dcf2f0af511
wangyiqiang <364514921@qq.com>
2016-06-13 16:53:36 +0800

    svn获取war包，目录为空时，应该置为失败

--- nebula-service/src/main/java/com/olymtech/nebula/service/action/GetPublishSvnAction.java
+++ nebula-service/src/main/java/com/olymtech/nebula/service/action/GetPublishSvnAction.java
@@ -8,7 +8,6 @@ import com.olymtech.nebula.core.action.AbstractAction;
 import com.olymtech.nebula.core.svn.SvnUtils;
 import com.olymtech.nebula.entity.NebulaPublishEvent;
 import com.olymtech.nebula.entity.enums.PublishAction;
-import com.olymtech.nebula.entity.enums.PublishActionGroup;
 import com.olymtech.nebula.service.IPublishScheduleService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,6 +15,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.tmatesoft.svn.core.wc.SVNClientManager;
+
+import java.io.File;
 
 /**
  * Created by Gavin on 2015-11-04 19:31.
@@ -35,25 +36,29 @@ public class GetPublishSvnAction extends AbstractAction {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    public GetPublishSvnAction(){
+    public GetPublishSvnAction() {
         super();
     }
 
     @Override
     public boolean doAction(NebulaPublishEvent event) throws Exception {
-        publishScheduleService.logScheduleByAction(event.getId(), PublishAction.GET_PUBLISH_SVN, event.getPublishActionGroup(), null, "");
+        publishScheduleService.logScheduleByAction(event.getId(), PublishAction.GET_PUBLISH_SVN, event
+                .getPublishActionGroup(), null, "");
         String svnUrl = event.getPublishSvn();
         SVNClientManager svnClientManager = SvnUtils.createSvnClientManager(svnUrl, DevSvnUsername, DevSvnPassword);
-        try{
-            Boolean svnResult = SvnUtils.checkout(svnClientManager, svnUrl, MasterDeployDir + event.getPublishProductKey() + "/publish_war/");
-            if(svnResult){
-                publishScheduleService.logScheduleByAction(event.getId(), PublishAction.GET_PUBLISH_SVN, event.getPublishActionGroup(), true, "");
+        try {
+            Boolean svnResult = SvnUtils.checkout(svnClientManager, svnUrl, MasterDeployDir + event
+                    .getPublishProductKey() + "/publish_war/");
+            if (svnResult) {
+                publishScheduleService.logScheduleByAction(event.getId(), PublishAction.GET_PUBLISH_SVN, event
+                        .getPublishActionGroup(), true, "");
                 return true;
             }
-        }catch (Exception e){
-            logger.error("GetPublishSvnAction error:",e);
+        } catch (Exception e) {
+            logger.error("GetPublishSvnAction error:", e);
         }
-        publishScheduleService.logScheduleByAction(event.getId(), PublishAction.GET_PUBLISH_SVN, event.getPublishActionGroup(), false, "获取发布war包时发生错误，可点击“重试”，尝试再次拉去。");
+        publishScheduleService.logScheduleByAction(event.getId(), PublishAction.GET_PUBLISH_SVN, event
+                .getPublishActionGroup(), false, "获取发布war包时发生错误，可点击“重试”，尝试再次拉去。");
         return false;
     }
 
@@ -64,6 +69,12 @@ public class GetPublishSvnAction extends AbstractAction {
 
     @Override
     public boolean doCheck(NebulaPublishEvent event) throws Exception {
+        String svnLocalPath = MasterDeployDir + event.getPublishProductKey() + "/publish_war/";
+        File file = new File(svnLocalPath);
+        File[] tempList = file.listFiles();
+        if (tempList.length == 0) {
+            return false;
+        }
         return true;
     }
 
